a70989dcbe50246391c75c71d5162afdb73424b6
Seydi Ahmet Kiyak <a.kiyak@openasapp.net>
2016-12-18 12:17:30 +0100

    Update ListComponent

--- client/imports/app/list/list.component.ts
+++ client/imports/app/list/list.component.ts
@@ -1,68 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import {
-    FormBuilder,
-    FormGroup,
-    Validators, FormArray
-} from '@angular/forms';
-import { Quiz, Question, Answer } from '../../../../both/models/quiz.model';
+import { Quiz } from '../../../../both/models/quiz.model';
 import { QuizCollection } from '../../../../both/collections/quiz.collection';
-import template from './create.component.html';
-import style from "./create.component.scss";
+import template from './list.component.html';
+import style from "./list.component.scss";
+import { Observable } from "rxjs/Observable";
 
 @Component({
-    selector: 'create',
+    selector: 'list',
     template,
     styles: [style]
 })
-export class CreateComponent implements OnInit {
+export class ListComponent implements OnInit {
 
-    quizForm: FormGroup;
+    quizList: Observable<any[]>;
 
-
-    constructor(private formBuilder: FormBuilder) { }
+    constructor() {
+        this.quizList = QuizCollection.find({}).zone();
+    }
 
     ngOnInit() {
-        this.quizForm = this.formBuilder.group({
-            name: ['', Validators.required],
-            questions: this.formBuilder.array([
-                this.initQuestionFormGroup()
-            ])
-        });
-
-        console.log(this.quizForm);
-    }
-
-    initQuestionFormGroup() {
-        return this.formBuilder.group({
-            question: ['', Validators.required],
-            answers: this.initAnswerFormGroup()
-        });
-    }
-
-
-    initAnswerFormGroup(): FormArray {
-        let answers: FormGroup[] = [];
-        for(let i = 0; i < 4; i++) {
-            answers.push(
-                this.formBuilder.group({
-                    answer: ['', Validators.required],
-                    right: [false, Validators.required]
-                })
-            )
-        }
-        return this.formBuilder.array(answers);
-    }
-
-    /**
-     * Adds more questions to the form
-     */
-    addQuestion() {
-        const control = <FormArray>this.quizForm.controls['questions'];
-        control.push(this.initQuestionFormGroup());
-    }
-
-    save(model: Quiz) {
-        console.log(model);
-        QuizCollection.insert(model);
     }
 }
