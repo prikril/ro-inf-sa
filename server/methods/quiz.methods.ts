fe51e127feec8bd90d001a00c00503f7b4317645
Seydi Ahmet Kiyak <s.ahmet.kiyak@gmail.com>
2017-01-06 15:43:03 +0100

    Use collection to get the id of the inserted quiz

--- server/methods/quiz.methods.ts
+++ server/methods/quiz.methods.ts
@@ -5,6 +5,6 @@ import {QuizCollection} from "../../both/collections/quiz.collection";
 
 Meteor.methods({
     saveQuiz: function(quizModel: Quiz) {
-        return QuizCollection.insert(quizModel);
+        return QuizCollection.collection.insert(quizModel);
     }
 });
