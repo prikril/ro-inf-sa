e7bc5e44645545ad1035ffbac553a021fec3b7ce
Bernhard Seidl <burzl-dev@online.de>
2017-01-19 03:16:02 +0100

    home screen aufgehübscht, diverse Änderungen am Styling

--- client/imports/app/home/home.component.ts
+++ client/imports/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import template from './home.component.html';
+import style from './home.component.scss';
 
 @Component({
-    selector: 'home',
-    template
+    template,
+    styles: [style]
 })
 export class HomeComponent implements OnInit {
     constructor() { }
