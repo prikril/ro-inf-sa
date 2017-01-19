8aaf0ef0aa06178c5990ee28195887c70ea788df
Seydi Ahmet Kiyak <s.ahmet.kiyak@gmail.com>
2017-01-19 21:05:15 +0100

    Add validator to validate the answers

--- client/imports/app/quizMaster/create/create.component.ts
+++ client/imports/app/quizMaster/create/create.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {
     FormBuilder,
     FormGroup,
-    Validators, FormArray
+    Validators, FormArray, FormControl
 } from '@angular/forms';
 import { Quiz } from '../../../../../both/models/quiz.model';
 import template from './create.component.html';
@@ -19,7 +19,6 @@ export class CreateComponent implements OnInit {
 
     quizForm: FormGroup;
 
-
     constructor(private formBuilder: FormBuilder, private router : Router) { }
 
     ngOnInit() {
@@ -31,14 +30,43 @@ export class CreateComponent implements OnInit {
         });
     }
 
+    /**
+     * Creates a FormGroup for a question with answers
+     * @returns {FormGroup}
+     */
     initQuestionFormGroup() {
         return this.formBuilder.group({
             question: ['', Validators.required],
             answers: this.initAnswerFormGroup()
+        }, {
+            validator: this.validateAnswer
         });
     }
 
+    validateAnswer(group: FormControl) {
+        let valid = false;
 
+        for(let answer of group.get('answers').value) {
+            if (answer['right']) {
+                valid = true;
+            }
+        }
+
+        if (valid) {
+            return null;
+        } else {
+            return {
+                validateAnswer: {
+                    valid: false
+                }
+            }
+        }
+    }
+
+    /**
+     * Creates a FormArray with four answers
+     * @returns {FormArray}
+     */
     initAnswerFormGroup(): FormArray {
         let answers: FormGroup[] = [];
         for(let i = 0; i < 4; i++) {
@@ -60,10 +88,19 @@ export class CreateComponent implements OnInit {
         control.push(this.initQuestionFormGroup());
     }
 
+    /**
+     * Call it before you check the right answer
+     * It will disable all other answers
+     * @param questionIndex the index of the question in the array of Questions
+     */
     checkRightAnswer(questionIndex: number) {
         this.uncheckAll(questionIndex);
     }
 
+    /**
+     * Sava the Quiz
+     * @param model
+     */
     save(model: Quiz) {
         MeteorObservable.call('saveQuiz', model).subscribe((quiz : Quiz) => {
             // Success Redirect ...
@@ -73,6 +110,10 @@ export class CreateComponent implements OnInit {
         });
     }
 
+    /**
+     * Unchecks all answer checkboxes from the given question index
+     * @param questionIndex the index of the question in the array of Questions
+     */
     private uncheckAll(questionIndex: number) {
         this.quizForm.get('questions');
         let answers = this.quizForm.get('questions').controls[questionIndex].get('answers').controls;
